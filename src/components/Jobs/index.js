ceb7b5ad3f53fb41230ce3ae467790e9df783615
saipranav333444 <saipranav333444@gmail.com>
2024-08-18 06:48:47 +0000

    first commit

--- src/components/Jobs/index.js
+++ src/components/Jobs/index.js
@@ -4,3 +4,90 @@ import {BsSearch} from 'react-icons/bs'
 import Loader from 'react-loader-spinner'
 
 import Header from '../Header'
+import ProfileDetails from '../ProfileDetails'
+import FiltersGroup from '../FiltersGroup'
+import JobCard from '../JobCard'
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+import './index.css'
+
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
+
+class Jobs extends Component {
+  state = {
+    profileDetails: {},
+    profileApiStatus: apiStatusConstants.initial,
+    jobsList: [],
+    jobsApiStatus: apiStatusConstants.initial,
+    searchInput: '',
+    activeSalaryRangeId: '',
+    employmentTypesChecked: [],
+  }
+
+  componentDidMount() {
+    this.getProfileDetails()
+    this.getJobs()
+  }
+
+  updateEmploymentTypesChecked = typeId => {
+    const {employmentTypesChecked} = this.state
+    let updatedList = employmentTypesChecked
+    if (employmentTypesChecked.includes(typeId)) {
+      updatedList = employmentTypesChecked.filter(
+        eachType => eachType !== typeId,
+      )
+    } else {
+      updatedList = [...updatedList, typeId]
+    }
+    this.setState({employmentTypesChecked: updatedList}, this.getJobs)
+  }
+
+  updateSalaryRangeId = activeSalaryRangeId =>
+    this.setState({activeSalaryRangeId}, this.getJobs)
+
+  getJobs = async () => {
+    this.setState({jobsApiStatus: apiStatusConstants.inProgress})
+
+    const {
+      activeSalaryRangeId,
+      employmentTypesChecked,
+      searchInput,
+    } = this.state
+    const employTypes = employmentTypesChecked.join(',')
+    const jwtToken = Cookies.get('jwt_token')
+    const apiUrl = `https://apis.ccbp.in/jobs?employment_type=${employTypes}&minimum_package=${activeSalaryRangeId}&search=${searchInput}`
+
+    const options = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+    const response = await fetch(apiUrl, options)
+    const data = await response.json()
+    if (response.ok === true) {
+      const {jobs} = data
+      const updatedData = jobs.map(eachJob => ({
+        companyLogoUrl: eachJob.company_logo_url,
+        employmentType: eachJob.employment_type,
+        id: eachJob.id,
+        jobDescription: eachJob.job_description,
+        location: eachJob.location,
+        packagePerAnnum: eachJob.package_per_annum,
+        rating: eachJob.rating,
+        title: eachJob.title,
+      }))
+      this.setState({
+        jobsList: updatedData,
+        jobsApiStatus: apiStatusConstants.success,
+      })
+    } else {
+      this.setState({jobsApiStatus: apiStatusConstants.failure})
+    }
+  }
+}
