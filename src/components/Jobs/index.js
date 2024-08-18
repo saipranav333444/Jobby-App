dfe69f0961b7112ed8b1a3941bbf88dedbf6e801
saipranav333444 <saipranav333444@gmail.com>
2024-08-18 11:19:43 +0000

    first commit

--- src/components/Jobs/index.js
+++ src/components/Jobs/index.js
@@ -90,4 +90,189 @@ class Jobs extends Component {
       this.setState({jobsApiStatus: apiStatusConstants.failure})
     }
   }
+
+  getProfileDetails = async () => {
+    this.setState({
+      profileApiStatus: apiStatusConstants.inProgress,
+    })
+
+    const jwtToken = Cookies.get('jwt_token')
+    const apiUrl = 'https://apis.ccbp.in/profile'
+
+    const options = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+
+    const response = await fetch(apiUrl, options)
+    const data = await response.json()
+
+    if (response.ok) {
+      const profileDetails = data.profile_details
+
+      const updatedData = {
+        name: profileDetails.name,
+        profileImageUrl: profileDetails.profile_image_url,
+        shortBio: profileDetails.short_bio,
+      }
+
+      this.setState({
+        profileDetails: updatedData,
+        profileApiStatus: apiStatusConstants.success,
+      })
+    } else {
+      this.setState({
+        profileApiStatus: apiStatusConstants.failure,
+      })
+    }
+  }
+
+  onSearchInput = event => {
+    this.setState({
+      searchInput: event.target.value,
+    })
+  }
+
+  renderSearchBar = searchBarID => {
+    const {searchInput} = this.state
+    return (
+      <div className="search-bar" id={searchBarID}>
+        <input
+          className="search-input"
+          type="search"
+          placeholder="Search"
+          value={searchInput}
+          onChange={this.onSearchInput}
+        />
+        <button
+          className="search-button"
+          type="button"
+          data-testid="searchButton"
+          onClick={() => this.getJobs()}
+          aria-label="button-text"
+        >
+          <BsSearch className="search-icon" />
+        </button>
+      </div>
+    )
+  }
+
+  renderSideBar = () => {
+    const {
+      profileDetails,
+      profileApiStatus,
+      activeSalaryRangeId,
+      employmentTypesChecked,
+    } = this.state
+
+    return (
+      <div className="side-bar">
+        {this.renderSearchBar('smallSearchBar')}
+        <ProfileDetails
+          profileDetails={profileDetails}
+          profileApiStatus={profileApiStatus}
+          getProfileDetails={this.getProfileDetails}
+        />
+        <hr className="separator" />
+        <FiltersGroup
+          updateSalaryRangeId={this.updateSalaryRangeId}
+          activeSalaryRangeId={activeSalaryRangeId}
+          updateEmploymentTypesChecked={this.updateEmploymentTypesChecked}
+          employmentTypesChecked={employmentTypesChecked}
+        />
+      </div>
+    )
+  }
+
+  renderNoJobsView = () => (
+    <div className="no-jobs-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/no-jobs-img.png"
+        alt="no jobs"
+        className="no-jobs-image"
+      />
+      <h1 className="no-jobs-heading">No Jobs Found</h1>
+      <p className="no-jobs-description">
+        We could not find any jobs. Try other filters.
+      </p>
+    </div>
+  )
+
+  renderJobsList = () => {
+    const {jobsList} = this.state
+    return (
+      <>
+        {jobsList.length > 0 ? (
+          <ul className="jobs-list">
+            {jobsList.map(eachJob => (
+              <JobCard key={eachJob.id} jobDetails={eachJob} />
+            ))}
+          </ul>
+        ) : (
+          this.renderNoJobsView()
+        )}
+      </>
+    )
+  }
+
+  renderJobsLoaderView = () => (
+    <div className="jobs-loader-container" data-testid="loader">
+      <Loader type="ThreeDots" color="#ffffff" height="50" width="50" />
+    </div>
+  )
+
+  renderJobsApiFailureView = () => (
+    <div className="jobs-api-failure-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/failure-img.png"
+        alt="failure view"
+        className="job-api-failure-image"
+      />
+      <h1 className="failure-view-heading">Oops! Something Went Wrong</h1>
+      <p className="failure-view-description">
+        We cannot seem to find the page you are looking for.
+      </p>
+      <button
+        type="button"
+        className="retry-button"
+        onClick={() => this.getJobs()}
+      >
+        Retry
+      </button>
+    </div>
+  )
+
+  renderJobsBasedOnAPiStatus = () => {
+    const {jobsApiStatus} = this.state
+
+    switch (jobsApiStatus) {
+      case apiStatusConstants.inProgress:
+        return this.renderJobsLoaderView()
+      case apiStatusConstants.success:
+        return this.renderJobsList()
+      case apiStatusConstants.failure:
+        return this.renderJobsApiFailureView()
+      default:
+        return null
+    }
+  }
+
+  render() {
+    return (
+      <div className="jobs-page-container">
+        <Header />
+        <div className="jobs-page">
+          {this.renderSideBar()}
+          <div className="jobs-container">
+            {this.renderSearchBar('largeSearchBar')}
+            {this.renderJobsBasedOnAPiStatus()}
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
+
+export default Jobs
